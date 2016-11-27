c667249e17ee33e2d1d8ef1eabe65a00c765ece9
zh <zh2015211203@bupt.edu.cn>
2016-11-27 22:37:30 +0800

    修改了一些错误

--- Graph/main.cpp
+++ Graph/main.cpp
@@ -3,38 +3,93 @@
 #include "Graph.h"
 using namespace std;
 
-void Loop_dfs(ALGraph,int,int*,int*,int);
-void Loop(ALGraph G);
+//void Loop1_dfs(ALGraph,int,int*,int*,int);
+//void Loop1(ALGraph G);
+void Loop1_dfs(ALGraph,int,int*);
+void Loop1(ALGraph G);
 int exist_path_dfs(ALGraph G,int i,int j,int k);
 
+/*
+*有些小问题的求回路算法
+*createed zh 2016.11.27 
+*/
+//int count = 0;
+//void Loop(ALGraph G){
+//	int loop[100],flag[100] = {0};
+//	for (int i = 0; i < G.vexnum; i++)
+//	    Loop_dfs(G,i,loop,flag,0);
+//}
+//
+//
+//void Loop_dfs(ALGraph G,int current,int loop[],int flag[],int depth){
+//	ArcNode* p = G.vertices[current].firstarc;
+//	flag[current] = 1;
+//  	loop[depth] = current;
+//  	while (p) {
+//    	if (p->adjvex == loop[0]) {
+//    		int i = 0;
+//      		cout << "第" << ++count << "条回路:";
+//      		for (; i <= depth; i++)
+//      			cout << G.vertices[loop[i]].data << "->";
+//      		cout << G.vertices[loop[0]].data << endl;
+//    	}else if (flag[p->adjvex] == 0) {
+//      		flag[p->adjvex] = 1;
+//      		Loop_dfs(G,p->adjvex,loop,flag,depth+1);
+//      		flag[p->adjvex] = 0;
+//    	}
+//    	p = p->nextarc;
+//  	}
+//}
+
+
+/*
+*改进后的求回路的算法
+*created by zh 2016.11.27
+*inspired by TX
+*ps 有问题别找我 -_-" 
+*/
+typedef struct {
+	int vex;
+	ArcNode *ptr;
+}VEX;
 
 int count = 0;
-void Loop(ALGraph G){
+void Loop1(ALGraph G){
 	int loop[100],flag[100] = {0};
 	for (int i = 0; i < G.vexnum; i++)
-	    Loop_dfs(G,i,loop,flag,0);
+		if (flag[i] == 0)
+	      Loop1_dfs(G,i,flag);
 }
 
-
-void Loop_dfs(ALGraph G,int current,int loop[],int flag[],int depth){
-	ArcNode* p = G.vertices[current].firstarc;
+void Loop1_dfs(ALGraph G,int current,int* flag){
+	VEX stack[100] = {-1,NULL},current_vex,next;
+	int index = 0;
+	current_vex.vex = current;
+	current_vex.ptr = G.vertices[current].firstarc;
+	stack[++index] = current_vex;
 	flag[current] = 1;
-  	loop[depth] = current;
-  	while (p) {
-    	if (p->adjvex == loop[0]) {
-    		int i = 0;
-      		cout << "第" << ++count << "条回路:";
-      		for (; i <= depth; i++)
-      			cout << G.vertices[loop[i]].data << "->";
-      		cout << G.vertices[loop[0]].data << endl;
-    	}else if (flag[p->adjvex] == 0) {
-      		flag[p->adjvex] = 1;
-      		Loop_dfs(G,p->adjvex,loop,flag,depth+1);
-      		flag[p->adjvex] = 0;
-    	}
-    	p = p->nextarc;
-  	}
-}
+	while (index){
+		current_vex = stack[index];
+		if ( current_vex.ptr && flag[current_vex.ptr->adjvex] == 1){
+			int i = 0;
+			for (; stack[i].vex != current_vex.ptr->adjvex; i++);
+			cout << "第" << ++count << "条回路:"; 
+			for (; i< index; i++){
+			 	cout << G.vertices[stack[i].vex].data << "->";
+			}
+			cout << G.vertices[stack[index].vex].data << endl;
+			stack[index].ptr = stack[index].ptr->nextarc;
+		}else if (current_vex.ptr){
+			next.vex = current_vex.ptr->adjvex;
+			next.ptr = G.vertices[next.vex].firstarc;
+			stack[index].ptr = stack[index].ptr->nextarc;
+			stack[++index] = next;
+			flag[next.vex] = 1;
+		}else{
+			index--;
+		}
+	}
+} 
 
 int visited[100];
 int exist_path_dfs(ALGraph G,int i,int j,int k) {
@@ -65,7 +120,7 @@ int main(){
 		switch (operate){
 			case 1:
 				CreateDALGraph(DG);
-				Loop(DG);
+				Loop1(DG);
 				break;
 			case 2:
 				CreateUDALGraph(UDG);
